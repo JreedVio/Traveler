e0c2dbef8231c6ffe47bd98406496ed65e10c65f
JreedVio <jreedvio1376@gmail.com>
2024-01-05 17:54:28 -0500

    Added time speed slider

--- Assets/_Scripts/UIManager.cs
+++ Assets/_Scripts/UIManager.cs
@@ -3,12 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _resourceText;
     [SerializeField] private TextMeshProUGUI _moneyText;
     [SerializeField] private Traveler _traveler;
+    
+    [SerializeField] private TextMeshProUGUI _timeSpeedText;
+    [SerializeField] private Slider _timeSpeedSlider;
     public static UIManager Instance { get; private set; }
     
 
@@ -19,7 +23,14 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
+        _timeSpeedSlider.onValueChanged.AddListener(OnValueChange);
         UpdateHUD();
+    }
+
+    void OnValueChange(float value)
+    {
+        _timeSpeedText.text = value.ToString();
+        Time.timeScale = value;
     }
 
     public void UpdateHUD()
